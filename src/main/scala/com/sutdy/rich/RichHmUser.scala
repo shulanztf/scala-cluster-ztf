ce5788c4e50ed1f7b612b6e65e9503de3810867a
1426763507@qq.com <1426763507@qq.com>
2017-12-11 13:07:09 +0800

    scala

---
+++ src/main/scala/com/sutdy/rich/RichHmUser.scala
@@ -0,0 +1,13 @@
+package com.sutdy.rich
+
+import com.sutdy.model.HmUser
+/**
+ * 隐式转换+比较
+ */
+class RichHmUser(val t: HmUser) extends Ordered[HmUser] {
+
+  override def compare(that: HmUser): Int = {
+    if (t.id > that.id) 1 else -1
+  }
+
+}
