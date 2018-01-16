c84d169cef0f506b2e6708f2e6dbcaba714dbd4f
1426763507@qq.com <1426763507@qq.com>
2018-01-16 11:45:16 +0800

    spark+scala

--- src/main/scala/com/sutdy/simple/WordCountFir.scala
+++ src/main/scala/com/sutdy/simple/WordCountFir.scala
@@ -16,12 +16,15 @@ object WordCountFir {
     println("aaa")
     if (args.length < 1) {
       System.err.println("无文件: <file>")
-      System.exit(1)
+//      TODO 无文件时，案例退出
+//      System.exit(1)
     }
+    val file1 = "D:\\data\\spark\\scala-word-count-1.txt";
+    
 
     val conf = new SparkConf()
     val sc = new SparkContext(conf)
-    val line = sc.textFile(args(0))
+    val line = sc.textFile(file1)
     //    分割字符，组装成map<key,1>
     val map = line.flatMap((file: String) => { file.split(" ") }).map((f: String) => { (f, 1) })
     //    .reduceByKey(func=>{fun})
