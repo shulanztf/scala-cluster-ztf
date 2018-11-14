d6ccbeb01c39fa65014761b0d29e6552e7608298
Administrator <admin@example.com>
2018-11-14 18:08:44 +0800

    spark+scala实例

---
+++ src/main/scala/com/sutdy/mr/Te1.scala
@@ -0,0 +1,10 @@
+package com.sutdy.mr
+
+/**
+  * @Description:
+  * @Auther: Administrator
+  * @Date: 2018/11/14 0014 17:18
+  */
+class Te1 {
+
+}
---
+++ src/main/scala/com/sutdy/mr/WeiboStatistics.scala
@@ -0,0 +1,10 @@
+package com.sutdy.mr
+
+/**
+  * Description:https://blog.csdn.net/liu16659/article/details/81254921 Spark案例实战之四
+  * Auther: Administrator
+  * Date: 2018/11/14 0014 17:56
+  */
+object WeiboStatistics {
+
+}
