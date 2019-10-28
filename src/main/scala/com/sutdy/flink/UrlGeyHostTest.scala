e806539f60d0710941ac25113f4d277a69bef29e
赵腾飞 <zhaotf@inspurworld.com>
2019-10-28 18:19:57 +0800

    资料整理

---
+++ src/main/scala/com/sutdy/flink/UrlGeyHostTest.scala
@@ -0,0 +1,9 @@
+package com.sutdy.flink
+
+/**
+  * @author 赵腾飞
+  * @date 2019/10/28/028 16:41
+  */
+class UrlGeyHostTest {
+
+}
---
+++ src/test/scala/ScalaTest1.scala
@@ -0,0 +1,17 @@
+/**
+  * @author 赵腾飞
+  * @date 2019/10/8/008 14:58
+  */
+class ScalaTest1 {
+
+}
+
+/**
+  *
+  */
+object ScalaTest1 {
+
+  def main(args: Array[String]): Unit = {
+    print("aaa", "......")
+  }
+}
