e0ad334bf41595c091ee202486046d94cda3c530
赵腾飞 <zhaotf@inspurworld.com>
2019-12-08 19:47:46 +0800

    资料整理，flink-sql双流join功能，batch版+stream版

--- scala-sample-flink/src/main/scala/com/api/study/flink/sql/TestSql5.scala
+++ scala-sample-flink/src/main/scala/com/api/study/flink/sql/TestSql5.scala
@@ -2,41 +2,44 @@ package com.api.study.flink.sql
 
 import org.apache.flink.api.scala._
 import org.apache.flink.core.fs.FileSystem.WriteMode
+import org.apache.flink.streaming.api.scala.{DataStream, StreamExecutionEnvironment}
 import org.apache.flink.table.api.scala._
 import org.apache.flink.table.api.{Table, TableEnvironment}
 import org.apache.flink.types.Row
-import org.slf4j.LoggerFactory
+import org.slf4j.{Logger, LoggerFactory}
 
 /**
   * @author 赵腾飞
-  * @date 2019/12/7/007 12:41
+  * @date 2019/12/8/008 19:33
   */
-class TestSql4 {
+class TestSql5 {
 
 }
 
 /**
-  * flink-sql 双流join,batch版
+  * flink-sql 双流join,stream版
   * https://blog.csdn.net/yanghua_kobe/article/details/73143367 Flink-Table-SQL系列之source
   */
-object TestSql4 {
-  val logger = LoggerFactory.getLogger(TestSql4.getClass)
+object TestSql5 {
+  val logger: Logger = LoggerFactory.getLogger(TestSql5.getClass)
 
   def main(args: Array[String]): Unit = {
-    val env = ExecutionEnvironment.getExecutionEnvironment
+    val env = StreamExecutionEnvironment.getExecutionEnvironment
     val tableEnv = TableEnvironment.getTableEnvironment(env)
 
     //    数据源
-    val source1: DataSet[(String, String)] = env.readTextFile("/data/flink/flink-data/person.txt").map(x => {
+    val source1: DataStream[(String, String)] = env.readTextFile("/data/flink/flink-data/person.txt").map(x => {
       val split = x.split("\t")
       (split(0), split(1))
     })
-    val source2: DataSet[(String, Int)] = env.readTextFile("/data/flink/flink-data/user.txt").map(x => {
+    val source2: DataStream[(String, Int)] = env.readTextFile("/data/flink/flink-data/user.txt").map(x => {
       val split = x.split("\t")
       (split(0), split(1).toInt)
     })
-    tableEnv.registerDataSet("table1", source1, 'name, 'score)
-    tableEnv.registerDataSet("table2", source2, 'name, 'level)
+
+    // 注册表、字段
+    tableEnv.registerDataStream("table1", source1, 'name, 'score)
+    tableEnv.registerDataStream("table2", source2, 'name, 'level)
     val table3: Table = tableEnv.sqlQuery(
       """
         |select t1.name as name, t1.score as score,
@@ -47,23 +50,12 @@ object TestSql4 {
         | where 1=1
       """.stripMargin)
 
-    val result3 = tableEnv.toDataSet[Row](table3) // 注意，必须Row,否则报java.lang.NullPointerException: Null result cannot be stored in a Case Class.
+    val result3: DataStream[(Boolean, Row)] = tableEnv.toRetractStream[Row](table3) // 注意，必须Row,否则报java.lang.NullPointerException: Null result cannot be stored in a Case Class.
     result3.writeAsText("/data/flink/file", WriteMode.OVERWRITE) //覆盖现有文件，生产慎用
       .setParallelism(1) //设置分区为1，避免多个文件
     //        result3.print() // 不要与execute并用，会有执行两次错误
 
-    env.execute("TestSql4")
+    env.execute("TestSql5")
   }
+
 }
-
-/**
-  * 定义样例类封装数据,模式匹配
-  *
-  * @param name
-  * @param score
-  */
-//case class Person3(name: String, score: String)
-//
-//case class User(name:String,level:Int)
-
-case class ResultTable(name: String, score: String, level: Int)
