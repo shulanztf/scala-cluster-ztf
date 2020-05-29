c62c5c6443b17f2e11c757fa91b43695c56bd378
赵腾飞 <zhaotf@inspurworld.com>
2020-05-29 21:22:04 +0800

    资料整理，flink事件时间+水位线

---
+++ scala-simple-flink/src/main/scala/com/msb/stream/EventTimeWordCount.scala
@@ -0,0 +1,54 @@
+package com.msb.stream
+
+import org.apache.flink.api.common.functions.ReduceFunction
+import org.apache.flink.streaming.api.TimeCharacteristic
+import org.apache.flink.streaming.api.scala.StreamExecutionEnvironment
+import org.apache.flink.streaming.api.scala.function.WindowFunction
+import org.apache.flink.streaming.api.scala._
+import org.apache.flink.streaming.api.windowing.time.Time
+import org.apache.flink.streaming.api.windowing.windows.TimeWindow
+import org.apache.flink.util.Collector
+
+/**
+ * @description: flink事件时间.
+ * @author: zhaotf
+ * @create: 2020-05-29 20:35
+ */
+object EventTimeWordCount {
+  def main(args: Array[String]): Unit = {
+    val env = StreamExecutionEnvironment.getExecutionEnvironment
+    //1、指定时间语义
+    env.setStreamTimeCharacteristic(TimeCharacteristic.EventTime)
+    //2、在往socket发射数据的时候 必须携带时间戳
+    val stream = env.socketTextStream("hserver133", 8888)
+      //3、assignAscendingTimestamps  指定哪一个值代表的是Event Time
+      .assignAscendingTimestamps(data => {
+        val splits = data.split(" ")
+        splits(0).toLong
+      })
+
+    stream
+      .flatMap(x => x.split(" ").tail)
+      .map((_, 1))
+      .keyBy(_._1)
+      //滚动窗口
+      .timeWindow(Time.seconds(3))
+      .reduce(new ReduceFunction[(String, Int)] {
+        override def reduce(value1: (String, Int), value2: (String, Int)): (String, Int) = {
+          (value1._1,value1._2 + value2._2)
+        }
+      },new WindowFunction[(String,Int),(String,Int),String,TimeWindow] {
+        override def apply(key: String, window: TimeWindow, input: Iterable[(String, Int)], out: Collector[(String, Int)]): Unit = {
+          val start = window.getStart
+          val end = window.getEnd
+          println("window start:" + start + "--- end:" + end)
+          for (elem <- input) {
+            out.collect(elem)
+          }
+        }
+      })
+      .print()
+
+    env.execute()
+  }
+}
